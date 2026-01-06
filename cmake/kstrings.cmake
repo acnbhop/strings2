83cdd9cc2e5a10e75b0fc5197af4f25d8c1fab17
grant <abernathy.grant@icloud.com>
2026-01-05 21:21:12 -0600

    CMake overhaul

---
+++ cmake/kstrings.cmake
@@ -0,0 +1,17 @@
+#
+# kstrings.cmake
+#
+#   Configuration for kstrings executable
+#
+
+add_executable(kstrings
+    src/main.cc
+)
+
+target_link_libraries(kstrings PRIVATE kstringslib)
+
+# Set the project name for configuration to apply the settings
+set(AAO_PROJECT_NAME kstrings)
+
+# Apply configuration settings
+include(cmake/config.cmake)
---
+++ cmake/kstringslib.cmake
@@ -0,0 +1,30 @@
+#
+# kstringslib.cmake
+#
+#   Configuration for kstringslib
+#
+
+add_library(kstringslib STATIC
+    src/kstrings/basics.cc
+    src/kstrings/basics.hh
+    src/kstrings/binary2strings.cc
+    src/kstrings/binary2strings.hh
+    src/kstrings/core.hh
+    src/kstrings/extracted-string.cc
+    src/kstrings/extracted-string.hh
+    src/kstrings/globals.hh
+    src/kstrings/memory-strings.cc
+    src/kstrings/memory-strings.hh
+    src/kstrings/module.cc
+    src/kstrings/module.hh
+    src/kstrings/print-buffer.cc
+    src/kstrings/print-buffer.hh
+    src/kstrings/string-parser.cc
+    src/kstrings/string-parser.hh
+)
+
+# Set the project name for configuration to apply the settings
+set(AAO_PROJECT_NAME kstringslib)
+
+# Apply configuration settings
+include(cmake/config.cmake)
